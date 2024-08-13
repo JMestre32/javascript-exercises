afb08e8c7ec418981b9ecf83ba2498084aa4637d
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2024-08-13 16:08:39 -0700

    Finished exericise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    try{
+        if(num < 0) throw "ERROR"
+        let new_str = "";
+        for(let j = 0; j < num; j++){
+            new_str = new_str + str;
+        }
+        return new_str;}
+    catch(err){
+        return err
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
