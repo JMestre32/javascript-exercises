05b43557e2a6dea15897bd2780f8550b5330eabb
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2024-08-15 10:06:23 -0700

    I'm getting good. WOO

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    //loop through the array
+    //if the current element is in targets, continue
+    //if not, add it to a new array
+    //return new array
+
+    let ret_Arr = []
+    for(let i = 0; i < arr.length; i++){
+        if(targets.includes(arr[i])){continue}
+        else{
+            ret_Arr.push(arr[i])
+        }
+    }
+    return ret_Arr
 };
 
 // Do not edit below this line
