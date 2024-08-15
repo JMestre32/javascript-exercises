71997913c33755bd652ec4f3ee8fedcdc222f541
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2024-08-15 09:37:43 -0700

    I think I'm him, wittawy

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    new_str = str.split("")
+    let ret_str = ""
+    
+    for(let i = str.length -1; i >= 0; i--){
+        ret_str = ret_str.concat("", new_str[i])
+    }
+    return ret_str
 };
 
 // Do not edit below this line
