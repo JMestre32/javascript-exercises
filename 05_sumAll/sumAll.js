4a993ff3a5a245f69364472e823a0d44b75367d0
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2024-08-15 11:03:38 -0700

    Finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if((typeof a === "number" && typeof b === "number") && (!((a < 0 || b < 0))) && (!( (a % 1 != 0 || b % 1 != 0)))){
+        if(a > b){
+            let temp = a
+            a = b
+            b = temp
+        }
+        for(let count = a + 1; count <= b; count++){
+            a = a + count
+        }
+        return a  
+    }
+    else{
+        return "ERROR"
+    }
 
 };
 
